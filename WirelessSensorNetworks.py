5a6e641c3ec9e424c65ea683b5d813e4e3acfe15
IceCereal <ns_raghav@hotmail.com>
2019-05-05 20:15:48 +0530

    Create Fitness Function

--- WirelessSensorNetworks.py
+++ WirelessSensorNetworks.py
@@ -38,3 +38,33 @@ if maxGateway > len(sensors):
 	print ("There are more gateways than the number of sensors")
 	print ("Exit")
 	exit()
+
+### Define:
+### Genetic/Evolutionary algorithms vars
+
+popSize = 12	# Population Size
+
+def Fitness(child, load = None, gamma = None):
+	"""
+		Fitness Function
+		
+		Inputs:
+			child (list):	Evaluate fitness on this child
+			load (list):	The Load on every Gateway: defaults to [30, 14, 20, 8]
+			gamma (int):	The constant: defaults to 0.25
+	"""
+
+	mean = 0	# Mean of the Loads
+	gamma = 0	# Gamma (a constant)
+	Load = []	# Loads on each Gateway. Can also be (num_bits * %_energy_in_gateway)
+	T_upper = 0	# Upper Threshold for granted number of gateways (mean + gamma*mean)
+	T_lower = 0	# Lower Threshold for granted number of gateways (mean - gamma*mean)
+	Load_Ratio = []	# Defined per each Gateway. Load_of_current_gateway / Highest_load
+	Exp_GLoad = 0 	# Expected Gateway Load = Sigma(Load_Ratio)/ Number_of_Gateways
+
+	Fitness = 0	# Fitness Function = Exp_GLoad * Num_Granted_Gateways / Number_of_Gateways
+
+	if load == None:
+		load = [30, 14, 20, 8]
+	if gamma == None:
+		gamma = 0.25
